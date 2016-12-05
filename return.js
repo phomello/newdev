0f305652b899918ada52f378d2afaa36c7fddf83
nicholas <letsoaloandrew458@gmail.com>
2016-12-05 16:17:25 +0200

    Delete return.js

--- return.js
+++
@@ -1,9 +0,0 @@
-$("input#nameted").on("click", function(){
-  var tname = $("input#name").val();
-  if($.trim(name) != ''){
-	  $.post("names.php", {tname: name}, function(data){
-		  $("#namedata").text(data);
-		  
-	  });
-  }
-});
